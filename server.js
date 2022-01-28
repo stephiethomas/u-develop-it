5824a28af9a3b2a21b20c22205575d836ce3a126
Stephanie Thomas <stephie.thomas@gmail.com>
2022-01-27 18:47:32 -0600

    create query

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+
+
 //connect to database
 const db = mysql.createConnection(
     {
@@ -18,11 +20,39 @@ const db = mysql.createConnection(
         database: 'election'
     },
     console.log('Connected to the election database.')
-);
+    );
+    
+    
+    // db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //     console.log(rows);
+    // });
 
+// get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
 
+db.query(sql,params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+});
 //Default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
